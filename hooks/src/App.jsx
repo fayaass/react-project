84841e6cd490f01633d6330bc386013eb4a910f8
fayaass <muhammedfayas59504@gmail.com>
2025-01-28 10:59:34 +0530

    added

---
+++ hooks/src/App.jsx
@@ -0,0 +1,95 @@
+import { useState, createContext, useContext } from "react";
+import ReactDOM from "react-dom/client";
+
+const UserContext = createContext();
+
+function Component1() {
+  const [user, setUser] = useState("Muhamme Fayas");
+
+  return (
+    <UserContext.Provider value={user}>
+      <h1>{`Hello ${user}!`}</h1>
+      <Component2 />
+    </UserContext.Provider>
+  );
+}
+
+function Component2() {
+  return (
+    <>
+      <h1>Component 2</h1>
+      <Component3 />
+    </>
+  );
+}
+
+function Component3() {
+  return (
+    <>
+      <h1>Component 3</h1>
+      <Component4 />
+    </>
+  );
+}
+
+function Component4() {
+  return (
+    <>
+      <h1>Component 4</h1>
+      <Component5 />
+    </>
+  );
+}
+
+function Component5() {
+  const user = useContext(UserContext);
+
+  return (
+    <>
+      <h1>Component 5</h1>
+      <h2>{`Hello ${user} again!`}</h2>
+    </>
+  );
+}
+export default Component1;
+
+
+
+
+
+
+
+
+
+// import { useState, useEffect, useRef } from "react";
+// import ReactDOM from "react-dom/client";
+
+// function App() {
+//   const [inputValue, setInputValue] = useState("");
+//   const previousInputValue = useRef("");
+
+//   useEffect(() => {
+//     previousInputValue.current = inputValue;
+//   }, [inputValue]);
+
+//   return (
+//     <>
+//       <input
+//         type="text"
+//         value={inputValue}
+//         onChange={(e) => setInputValue(e.target.value)}
+//       />
+//       <h2>Current Value: {inputValue}</h2>
+//       <h2>Previous Value: {previousInputValue.current}</h2>
+//     </>
+//   );
+// }
+// export default App;
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(<App />);
+
+// /*
+// Start typing in the input field and you will
+// see the current- and previous value.
+// */
